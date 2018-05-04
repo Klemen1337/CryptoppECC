0e79cb3e1fd469bbcff08056f3b29e65cf66d05f
Klemen Kastelic <klemen.kast@gmail.com>
2018-05-04 13:52:00 +0200

    Updated builder (removed macOS, and iPhone simulator i386 arch)

--- CryptoppECC/CryptoppLibrary/builder.sh
+++ CryptoppECC/CryptoppLibrary/builder.sh
@@ -5,9 +5,10 @@ cd CryptoppECC/CryptoppLibrary
 XCODE_ROOT=`xcode-select -print-path`
 
 echo "making for iOS"
-ARCHS="x86_64 i386 armv7 arm64"
+# i386 not working
+ARCHS="x86_64 armv7 arm64"
 SDK_VERSION=`xcrun --sdk iphoneos --show-sdk-version 2> /dev/null`
-MIN_SDK_VERSION=7.0
+MIN_SDK_VERSION=9.0
 
 
 echo "sdk version is: "
@@ -35,79 +36,79 @@ do
     export BUILD_PATH="IOS_BUILD_${ARCH}"
     export CXXFLAGS="-x c++ -arch ${ARCH} -isysroot ${SDK_ROOT} -I${BUILD_PATH} -miphoneos-version-min=${MIN_SDK_VERSION} -mios-simulator-version-min=${MIN_SDK_VERSION}"
 
+    make clean
     mkdir -pv ${BUILD_PATH}
-    make -f Makefile
-    mv *.o ${BUILD_PATH}
-    mv *.d ${BUILD_PATH}
+    make -f GNUmakefile
     mv libcryptopp.a ${BUILD_PATH}
 
     IOS_STATIC_ARCHIVES="${IOS_STATIC_ARCHIVES} ${BUILD_PATH}/libcryptopp.a"
 
 done
 
+make clean
 echo "Creating universal library..."
- mkdir -p bin/ios
- lipo -create ${IOS_STATIC_ARCHIVES} -output bin/ios/libcryptopp.a
+mkdir -p bin/ios
+lipo -create ${IOS_STATIC_ARCHIVES} -output bin/ios/libcryptopp.a
 
 echo "removing thin archs"
 for ARCH in ${ARCHS}
 do
 
-    directoryName=IOS_BUILD_${ARCH}
-    rm -rf ${directoryName}
+   directoryName=IOS_BUILD_${ARCH}
+   rm -rf ${directoryName}
 
 done
 
 
 ############################################################
-echo "making for macOSX"
-ARCHS="x86_64 i386"
-SDK_VERSION=`xcrun --sdk macosx --show-sdk-version 2> /dev/null`
-MIN_SDK_VERSION=10.10
-PLATFORM="MacOSX"
+# echo "making for macOSX"
+# ARCHS="x86_64 i386"
+# SDK_VERSION=`xcrun --sdk macosx --show-sdk-version 2> /dev/null`
+# MIN_SDK_VERSION=10.10
+# PLATFORM="MacOSX"
 
 
-echo "sdk version is: "
-echo ${SDK_VERSION}
+# echo "sdk version is: "
+# echo ${SDK_VERSION}
 
 
-MACOSX_STATIC_ARCHIVES=""
-for ARCH in ${ARCHS}
-do
+# MACOSX_STATIC_ARCHIVES=""
+# for ARCH in ${ARCHS}
+# do
 
-export DEV_ROOT="${XCODE_ROOT}/Platforms/${PLATFORM}.platform/Developer"
-export SDK_ROOT="${DEV_ROOT}/SDKs/${PLATFORM}${SDK_VERSION}.sdk"
-export TOOLCHAIN_ROOT="${XCODE_ROOT}/Toolchains/XcodeDefault.xctoolchain/usr/bin/"
-export CC="clang -arch $ARCH -fembed-bitcode"
-export CXX=clang++
-export AR=${TOOLCHAIN_ROOT}libtool
-export RANLIB=${TOOLCHAIN_ROOT}ranlib
-export ARFLAGS="-static -o"
-export LDFLAGS="-arch ${ARCH} -isysroot ${SDK_ROOT}"
-export BUILD_PATH="MACOSX_BUILD_${ARCH}"
-export CXXFLAGS="-x c++ -arch ${ARCH} -isysroot ${SDK_ROOT} -I${BUILD_PATH} -mmacosx-version-min=${MIN_SDK_VERSION}"
+# export DEV_ROOT="${XCODE_ROOT}/Platforms/${PLATFORM}.platform/Developer"
+# export SDK_ROOT="${DEV_ROOT}/SDKs/${PLATFORM}${SDK_VERSION}.sdk"
+# export TOOLCHAIN_ROOT="${XCODE_ROOT}/Toolchains/XcodeDefault.xctoolchain/usr/bin/"
+# export CC="clang -arch $ARCH -fembed-bitcode"
+# export CXX=clang++
+# export AR=${TOOLCHAIN_ROOT}libtool
+# export RANLIB=${TOOLCHAIN_ROOT}ranlib
+# export ARFLAGS="-static -o"
+# export LDFLAGS="-arch ${ARCH} -isysroot ${SDK_ROOT}"
+# export BUILD_PATH="MACOSX_BUILD_${ARCH}"
+# export CXXFLAGS="-x c++ -arch ${ARCH} -isysroot ${SDK_ROOT} -I${BUILD_PATH} -mmacosx-version-min=${MIN_SDK_VERSION}"
 
-mkdir -pv ${BUILD_PATH}
-make -f Makefile
-mv *.o ${BUILD_PATH}
-mv *.d ${BUILD_PATH}
-mv libcryptopp.a ${BUILD_PATH}
+# mkdir -pv ${BUILD_PATH}
+# make -f GNUmakefile
+# mv *.o ${BUILD_PATH}
+# mv *.d ${BUILD_PATH}
+# mv libcryptopp.a ${BUILD_PATH}
 
-MACOSX_STATIC_ARCHIVES="${MACOSX_STATIC_ARCHIVES} ${BUILD_PATH}/libcryptopp.a"
+# MACOSX_STATIC_ARCHIVES="${MACOSX_STATIC_ARCHIVES} ${BUILD_PATH}/libcryptopp.a"
 
-done
+# done
 
-echo "Creating universal library..."
-mkdir -p bin/macosx
-lipo -create ${MACOSX_STATIC_ARCHIVES} -output bin/macosx/libcryptopp.a
+# echo "Creating universal library..."
+# mkdir -p bin/macosx
+# lipo -create ${MACOSX_STATIC_ARCHIVES} -output bin/macosx/libcryptopp.a
 
-echo "removing thin archs"
-for ARCH in ${ARCHS}
-do
+# echo "removing thin archs"
+# for ARCH in ${ARCHS}
+# do
 
-directoryName=MACOSX_BUILD_${ARCH}
-rm -rf ${directoryName}
+# directoryName=MACOSX_BUILD_${ARCH}
+# rm -rf ${directoryName}
 
-done
+# done
 
 echo "Build done!"
